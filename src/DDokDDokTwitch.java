a63788a64cca8027d33ecd83a8c9daf69fb41b8b
Kim SangHeon <comnamu18@gmail.com>
2019-05-21 13:32:32 +0900

    IRC Connection
    Refacoring Done

--- src/DDokDDokTwitch.java
+++ src/DDokDDokTwitch.java
@@ -1,23 +1,25 @@
-import net.engio.mbassy.listener.Handler;
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.kitteh.irc.client.library.Client;
 import org.kitteh.irc.client.library.event.channel.ChannelJoinEvent;
 import org.kitteh.irc.client.library.event.channel.ChannelMessageEvent;
 import org.kitteh.irc.client.library.feature.twitch.TwitchSupport;
 
+import net.engio.mbassy.listener.Handler;
 
-public class test1 {
+public class DDokDDokTwitch {
 	public static final String BOTNAME = "ddok_ddok";
 	//public static final String CLIENTID = "6v4v3829v8ksmmbf6snrjm5la556pd";
-	public static final String CHANNEL = "#hana5837";
-	
+	public static String CHANNEL = "#hana5837";
 	public static ArrayList<String> userLogged;
-	
 	public static int test = 0;
 	
-    public static class Listener {
+	public static class Listener {
         @Handler
         public void onUserJoinChannel(ChannelJoinEvent event) {
         	String msgUser = event.getUser().getNick();
@@ -53,11 +55,15 @@ public class test1 {
         }
     }
 
-
-	public static void main(String[] args) throws Exception {
+	public DDokDDokTwitch(String CHANNEL, String UserName) {
+		this.CHANNEL = CHANNEL;
 		userLogged = new ArrayList<>();
 		userLogged.add(BOTNAME);
-		
+		userLogged.add(UserName);
+	}
+	
+	public void connect() {
+		String OAUTH = oauthString();
         Client client = Client.builder()
                 .server().host("irc.chat.twitch.tv").port(443)
                 .password(OAUTH).then()
@@ -67,6 +73,26 @@ public class test1 {
         client.connect();
         client.getEventManager().registerEventListener(new Listener());
         client.addChannel(CHANNEL);
-        
+	}
+	
+	public String oauthString() {
+		String ret;
+		try {
+			String file = "./Resources/oauth.json";
+			InputStream is = DDokDDokTwitch.class.getResourceAsStream(file);
+	        if (is == null) {
+	            throw new NullPointerException("Cannot find resource file " + file);
+	        }
+	        
+	        JSONTokener tokener = new JSONTokener(is);
+	        JSONObject jsonObject = new JSONObject(tokener);
+	   
+	        ret = (String) jsonObject.get("OAUTH");
+		} catch (Exception e) {
+			e.printStackTrace();
+			ret = null;
+		}
+		System.out.println(ret);
+		return ret;
 	}
 }
